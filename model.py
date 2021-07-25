da9801252f117a831651170f72aab9b56e1263d1
Grace Birnam <grace.mbirnam@gmail.com>
2021-07-24 18:02:07 -0700

    started API gets

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
@@ -50,9 +50,26 @@ class UsersRecipes(db.Model):
     allergy = db.relationship('Allergies', backref='allergies')
 
 
-# if __name__ ==  '__main__':
-#     from server import app
 
-#     connect_to_db(app)
+
+
+
+
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///tinyOrganics', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+if __name__ ==  '__main__':
+    from server import app
+
+    connect_to_db(app)
     
 
